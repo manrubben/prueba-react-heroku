604a3bf02d70cf56ab7679a2bd7661a04650f951
Manu <manrubben@alum.us.es>
2022-03-11 18:59:30 +0100

    Login method done

--- server/index.js
+++ server/index.js
@@ -31,6 +31,29 @@ app.post("/register", (req, res) => {
 });
 
 
+//Login method
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query(
+        "SELECT * FROM users WHERE username = ? AND password = ?",
+        [username, password],
+        (err, result) => {
+            if (err) {
+                res.send({ err: err });
+            }
+
+            if (result.length > 0) {
+                res.send(result);
+            } else {
+                res.send({ message: "Wrong username/password combination!"})
+            }
+        }
+    );
+});
+
+
 // List method
 app.get("/api/get", (req, res) => {
 
