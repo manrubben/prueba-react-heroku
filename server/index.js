313fe10e0366a9ca310551c65a442fe1b40e0fd8
Manu <manrubben@alum.us.es>
2022-03-16 20:29:24 +0100

    Login with session and cookie done

--- server/index.js
+++ server/index.js
@@ -4,21 +4,44 @@ const cors = require("cors");
 const app = express();
 const mysql = require('mysql');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
+
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const PORT = 3001;
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: "eu-cdbr-west-02.cleardb.net",
     user: "b976cfa96691f2",
     password: "e1b56bb4",
     database: "heroku_cf5c4e9e2e2106c",
 });
 
-app.use(cors());
+db.query('select 1 + 1', (err, rows) => { /* */ }); //para mantener la conexion activa con la base de datos del servidor
+
+
 app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        key: "userId",
+        secret: "subscribe",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 60 * 60 * 24,
+        },
+    })
+);
 
 
 // Register method
@@ -43,6 +66,16 @@ app.post("/register", (req, res) => {
 });
 
 
+//Method to check if a user is logged in
+app.get("/login", (req, res) => {
+    if(req.session.user) {
+        res.send({ loggedIn: true, user: req.session.user });
+    } else {
+        res.send({ loggedIn: false });
+    }
+});
+
+
 //Login method
 app.post("/login", (req, res) => {
     const username = req.body.username;
@@ -59,6 +92,8 @@ app.post("/login", (req, res) => {
             if (result.length > 0) {
                 bcrypt.compare(password, result[0].password, (error, response) => {
                     if(response) {
+                        req.session.user = result;
+                        console.log(req.session.user);
                         res.send(result);
                     } else {
                         res.send({ message: "Wrong username/password combination!"})
